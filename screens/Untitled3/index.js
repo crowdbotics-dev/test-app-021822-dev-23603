b146da2778a5dd0a21e245a4b27fb65da204ed8f
Aline Tavares <aline@crowdbotics.com>
2022-04-19 10:45:58 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -8,7 +8,16 @@ const Untitled3 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      </View>;
+      <View style={{
+      left: -17.19915771484375,
+      top: 57,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View></View>;
 };
 
 export default Untitled3;

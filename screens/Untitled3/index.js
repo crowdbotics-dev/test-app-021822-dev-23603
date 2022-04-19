16ee167592cae43a74b06b5ba87bf94a9d1e9b3d
Aline Tavares <aline@crowdbotics.com>
2022-04-19 11:00:36 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -9,8 +10,26 @@ const Untitled3 = () => {
     height: '100%'
   }}>
       <View style={{
-      left: -17.19915771484375,
-      top: 57,
+      left: 57,
+      top: 292,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><Text style={{
+      left: 32,
+      top: 40,
+      position: "absolute",
+      width: 100,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>Lorem ipsum… </Text><View style={{
+      left: 57.01704406738281,
+      top: 138,
       position: "absolute",
       height: 60,
       width: 140,
